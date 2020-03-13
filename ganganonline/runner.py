d08b1d2852434493207020e410b812f7e88a1c6c
umjammer <umjammer@gmail.com>
2020-03-13 12:40:46 +0900

    add gangan online

---
+++ ganganonline/runner.py
@@ -0,0 +1,101 @@
+# --- coding: utf-8 ---
+"""
+ganganonline の実行クラスモジュール
+"""
+
+import sys
+from os import path
+from datetime import datetime
+from runner import AbstractRunner
+from ganganonline.login import YahooLogin
+from ganganonline.manager import Manager
+
+
+class Runner(AbstractRunner):
+    """
+    ganganonline の実行クラス
+    https://viewer.ganganonline.com/manga/?chapterId=15502
+    """
+
+    OPTION_BOUND_ON_LEFT_SIDE = 'L'
+    """
+    左綴じを示すオプション
+    """
+
+    OPTION_BOUND_ON_RIGHT_SIDE = 'R'
+    """
+    右綴じを示すオプション
+    """
+
+    domain_pattern = 'viewer\\.ganganonline\\.com'
+    """
+    サポートするドメイン
+    """
+
+    patterns = ['manga\\/\\?chapterId=\\d+']
+    """
+    サポートする ganganonline のパスの正規表現のパターンのリスト
+    """
+
+    is_login = False
+    """
+    ログイン状態
+    """
+
+    def run(self):
+        """
+        ganganonline の実行
+        """
+        try:
+            if (self.config.ebookjapan.needs_login and
+                    not self._is_login() and not self._login()):
+                return
+        except Exception as err:
+            _filename = 'login_error_%s.png' % datetime.now().strftime('%s')
+            self.browser.driver.save_screenshot(
+                path.join(self.config.log_directory, _filename))
+            print('ログイン時にエラーが発生しました: %s' %
+                  err.with_traceback(sys.exc_info()[2]))
+            return
+        print('Loading page of inputed url (%s)' % self.url)
+        self.browser.visit(self.url)
+
+        _destination = self.url[self.url.rindex('chapterId=') + 10:]
+        print(f'Output Path : {_destination}')
+#        _destination = input('Output Path > ')
+        _manager = Manager(
+            self.browser, self.config.ganganonline, _destination)
+        _result = _manager.start()
+        if _result is not True:
+            print(_result)
+        return
+
+    def _is_login(self):
+        """
+        ログイン状態の確認を行う
+        @return ログインしている場合に True を、していない場合に False を返す
+        """
+        if Runner.is_login:
+            return True
+        self.browser.visit(self.url)
+        if len(self.browser.find_by_css('.login')) == 0:
+            Runner.is_login = True
+            return True
+        return False
+
+    def _login(self):
+        """
+        ログイン処理を行う
+        @return ログイン成功時に True を返す
+        """
+        if self.config.ganganonline.username and self.config.ganganonline.password:
+            yahoo = YahooLogin(
+                self.browser,
+                self.config.ganganonline.username,
+                self.config.ganganonline.password)
+        else:
+            yahoo = YahooLogin(self.browser)
+        if yahoo.login():
+            Runner.is_login = True
+            return True
+        return False
