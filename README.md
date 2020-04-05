153ddc7f3a6a48f3c0db8775693db51692892bab
umjammer <umjammer@gmail.com>
2020-04-05 19:39:54 +0900

    add about booklive

--- README.md
+++ README.md
@@ -14,13 +14,13 @@ Types available.
 |[ebookjapan](http://ebookjapan.yahoo.co.jp/)|paging|screen capture|manual|need to specify size for every volume|[⚓](https://ebookjapan.yahoo.co.jp/books/145222/A000100547)|
 |[bookpass]()|paging|canvas data|auto| |[⚓](https://bookpass.auone.jp/pack/detail/?iid=BT000069318400100101&cs=top_freecomics_reco_670&pos=2&tab=1&ajb=3)|
 |[bookwalker]()|paging|canvas data|auto| |[⚓](https://viewer.bookwalker.jp/browserWebApi/03/view?cid=57c84cf2-7062-4ef9-9071-45fb249c926e)|
-|[comicdays]()|meta|direct file (re-rendered)|auto| |[⚓](https://comic-days.com/volume/13932016480030155016)|
-|[comicwalker]()|meta|direct file (decrypted)|auto| |[⚓](https://comic-walker.com/viewer/?tw=2&dlcl=ja&cid=KDCW_MF09000001010005_68)|
+|[comicdays](https://comic-days.com/)|meta|direct file (re-rendered)|auto| |[⚓](https://comic-days.com/volume/13932016480030155016)|
+|[comicwalker](https://comic-walker.com/)|meta|direct file (decrypted)|auto| |[⚓](https://comic-walker.com/viewer/?tw=2&dlcl=ja&cid=KDCW_MF09000001010005_68)|
 |[ganganonline]()|paging|direct file (blob)|auto| |[⚓](https://viewer.ganganonline.com/manga/?chapterId=15502)|
-|[linemanga]()|paging|screen capture|auto| |[⚓](https://manga.line.me/book/viewer?id=92dc0b4e-c5d4-4518-9fba-d78fb1e6b0f0)|
-|[webace]()|meta|direct file|auto| |[⚓](https://web-ace.jp/youngaceup/contents/1000053/episode/1092/)|
-|[zebrackcomic]()|paging|direct file (blob)|auto| |[⚓](https://zebrack-comic.com/title/37/volume/1498/viewer)|
-
+|[linemanga](https://manga.line.me/)|paging|screen capture|auto|need to update cookie|[⚓](https://manga.line.me/book/viewer?id=92dc0b4e-c5d4-4518-9fba-d78fb1e6b0f0)|
+|[webace](https://web-ace.jp/)|meta|direct file|auto| |[⚓](https://web-ace.jp/youngaceup/contents/1000053/episode/1092/)|
+|[zebrackcomic](https://zebrack-comic.com/)|paging|direct file (blob)|auto| |[⚓](https://zebrack-comic.com/title/37/volume/1498/viewer)|
+|[booklive](https://booklive.jp/)|paging|direct file (blob, re-rendered)|auto| |[⚓]('https://booklive.jp/bviewer/s/?cid=208562_003&rurl=https%3A%2F%2Fbooklive.jp%2Findex%2Fno-charge%2Fcategory_id%2FC)|
 ## Requirement
 
 * `Python` Python 3 (tested on 3.7.6)
---
+++ booklive/manager.py
@@ -0,0 +1,121 @@
+# --- coding: utf-8 ---
+"""
+booklive の操作を行うためのクラスモジュール
+"""
+
+import io
+import re
+import time
+from selenium.webdriver.common.keys import Keys
+from manager import AbstractManager, get_file_content_chrome
+from PIL import Image
+
+
+class Manager(AbstractManager):
+    """
+    booklive の操作を行うためのクラス
+    """
+
+    def __init__(self, browser, config=None, directory='./', prefix=''):
+        """
+        bookpass の操作を行うためのコンストラクタ
+        @param browser splinter のブラウザインスタンス
+        """
+        super().__init__(browser, config, directory, prefix)
+
+        self.next_key = Keys.ARROW_LEFT
+        """
+        次のページに進むためのキー
+        """
+        self.current_page_element = None
+        """
+        現在表示されているページのページ番号が表示されるエレメント
+        """
+
+    def start(self, url=None):
+        """
+        ページの自動スクリーンショットを開始する
+        @return エラーが合った場合にエラーメッセージを、成功時に True を返す
+        """
+        self.browser.driver.set_window_size(480, 640)
+
+        self._wait()
+
+        _total = self._get_total_page()
+        if not _total:
+            return '全ページ数の取得に失敗しました'
+
+        self.current_page_element = self._get_current_page_element()
+        if self.current_page_element is None:
+            return '現在のページ情報の取得に失敗しました'
+
+        self._set_total(_total)
+        for _count in range(0, _total):
+
+            _imgs = self.browser.find_by_css(f"#content-p{_count + 1} div.pt-img img")
+            _images = [self._get_image(_img._element.get_attribute('src')) for _img in _imgs]
+            # print(f'images: {len(_images)}')
+            _w = _images[0].size[0]
+            _h = sum(_image.size[1] for _image in _images)
+            _dest = Image.new('RGB', (_w, _h))
+            _hh = 0
+            for _image in _images:
+                _dest.paste(_image, (0, _hh, _w, _hh + _image.size[1]))
+                _hh += _image.size[1]
+            self._save_image(_count, _dest)
+
+            self.pbar.update(1)
+
+            self._next()
+            self._sleep()
+
+        return True
+
+    def _get_image(self, url):
+        _image = Image.open(io.BytesIO(get_file_content_chrome(self.browser.driver, url)))
+        if self._is_config_jpeg():
+            _image = _image.convert('RGB')
+        return _image
+
+    def _get_total_page(self):
+        """
+        全ページ数を取得する
+        最初にフッタの出し入れをする
+        @return 取得成功時に全ページ数を、失敗時に None を返す
+        """
+        for _ in range(Manager.MAX_LOADING_TIME):
+            _elements = self.browser.find_by_css('#menu_slidercaption')
+            if len(_elements) != 0:
+                print(_elements.first.html)
+                if re.match('^\\d+/\\d+$', _elements.first.html):
+                    return int(_elements.first.html.split('/')[1])
+            time.sleep(1)
+        return None
+
+    def _get_current_page_element(self):
+        """
+        現在表示されているページのページ数が表示されているエレメントを取得する
+        @return ページ数が表示されているエレメントがある場合はそのエレメントを、ない場合は None を返す
+        """
+        _elements = self.browser.find_by_css('#menu_slidercaption')
+        if len(_elements) != 0:
+            return _elements.first
+        print("no current")
+        return None
+
+    def _get_current_page(self):
+        """
+        現在のページを取得する
+        @return 現在表示されているページ
+        """
+        return int(self.current_page_element.html.split('/')[0])
+
+    def _next(self):
+        """
+        次のページに進む
+        """
+        _current_page = self._get_current_page()
+        self._press_key(self.next_key)
+        if self._get_current_page() < self.pbar.total - 1:
+            while self._get_current_page() != _current_page + 1:
+                time.sleep(0.1)
